7b4e317440140259800bcb158895e4467817aaa3
ambientcowboy <holysignals@protonmail.com>
2026-02-06 20:59:56 -0800

    Added lines 25-31, for loop

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ class Program
     static void Main(string[] args)
     {
         int taskNumber = 1;
-//this loop displays/prints a message for task number inputs 1-5
+        //this loop displays/prints a message for task number inputs 1-5
         while (taskNumber <= 5) 
         {
             Console.WriteLine($"Task {taskNumber} completed");
@@ -13,7 +13,8 @@ class Program
         }
 
         int attempt = 1;
-        /* this do-while loop checks the set condition after the program runs the loop body at lease once,
+        /*
+         this do-while loop checks the set condition after the program runs the loop body at lease once,
          this ensures that the code within the loop body executes at least once.
          This execution occurs even if the condition set is initially false.
          */
@@ -22,5 +23,11 @@ class Program
             Console.WriteLine($"Attempt {attempt}");
             attempt++;
         } while (attempt <= 3);
+
+        //We use a for loop for this task because we know the number of iterations before programming the loop.
+        for (int step = 1; step <= 4; step++)
+        {
+            Console.WriteLine($"Step {step}");
+        }
     }
 }
