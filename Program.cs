c891f5f0be88874e283393c5616b1eb70d91d73c
ambientcowboy <holysignals@protonmail.com>
2026-02-06 20:22:02 -0800

    Fixed Loop bug, removed ; on line 11 Removed bool from line 8

--- Program.cs
+++ Program.cs
@@ -5,9 +5,10 @@ class Program
     static void Main(string[] args)
     {
         int taskNumber = 1;
-        bool isCorrect = false;
+        
+        
         //this loop displays/prints a message for task number inputs 1-5
-        while (taskNumber <= 5) ;
+        while (taskNumber <= 5) 
         {
             Console.WriteLine($"Task {taskNumber} completed");
             taskNumber++;
