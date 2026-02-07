eb66dceb02a799f390b180cf9018b3dc1f6c7a61
ambientcowboy <holysignals@protonmail.com>
2026-02-06 20:14:05 -0800

    Added lines 6-15, int variable and while loop

--- Program.cs
+++ Program.cs
@@ -6,6 +6,9 @@ class Program
     {
         int taskNumber = 1;
         bool isCorrect = false;
+        
+        //this loop displays/prints a message for task number inputs 1-5
+        
         while (taskNumber <= 5) ;
         {
             Console.WriteLine($"Task {taskNumber} completed");
