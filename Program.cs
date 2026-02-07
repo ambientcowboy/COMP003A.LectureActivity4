fbc1818697497f936030c2e0d4d49861c399b454
ambientcowboy <holysignals@protonmail.com>
2026-02-06 20:46:09 -0800

    Added lines 14-24, int attempt and do-while loop

--- Program.cs
+++ Program.cs
@@ -5,13 +5,22 @@ class Program
     static void Main(string[] args)
     {
         int taskNumber = 1;
-        
-        
-        //this loop displays/prints a message for task number inputs 1-5
+//this loop displays/prints a message for task number inputs 1-5
         while (taskNumber <= 5) 
         {
             Console.WriteLine($"Task {taskNumber} completed");
             taskNumber++;
         }
+
+        int attempt = 1;
+        /* this do-while loop checks the set condition after the program runs the loop body at lease once,
+         this ensures that the code within the loop body executes at least once.
+         This execution occurs even if the condition set is initially false.
+         */
+        do 
+        {
+            Console.WriteLine($"Attempt {attempt}");
+            attempt++;
+        } while (attempt <= 3);
     }
 }
